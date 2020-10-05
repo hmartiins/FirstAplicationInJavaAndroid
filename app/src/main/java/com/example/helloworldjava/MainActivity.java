8210a5ad3483c28ff07ce8efbd816b5e37352bbc
Henrique Martins <hmartins224@gmail.com>
2020-10-05 12:50:25 -0300

    Searching the user's contact list through ContentProvider

--- app/src/main/java/com/example/helloworldjava/MainActivity.java
+++ app/src/main/java/com/example/helloworldjava/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.helloworldjava;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.provider.ContactsContract;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.CursorAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +32,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.i("Activity Status", "onCreate: ");
+
+        listView = findViewById(R.id.lstContacts);
+        arrayList = new ArrayList<>();
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, arrayList);
+
+        listView.setAdapter(arrayAdapter);
+
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_CONTACTS},100);
+        } else {
+            readContacts();
+        }
     }
 
     @Override
@@ -71,4 +97,28 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
         Log.i("Activity Status",  "onDestroy: ");
     }
+
+    ListView listView;
+    ArrayList<String> arrayList;
+    ArrayAdapter arrayAdapter;
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        readContacts();
+    }
+
+    private void readContacts(){
+        ContentResolver contentResolver = getContentResolver();
+        Cursor cursor = contentResolver.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null, null);
+
+        if(cursor.moveToFirst()){
+            do{
+                arrayList.add(cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME)));
+            } while (cursor.moveToNext());
+            arrayAdapter.notifyDataSetChanged();
+        }
+
+    }
+
 }
